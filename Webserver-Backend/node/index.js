6c5952dccb7e2ce6cba710d99071a4e0207d290d
puze <puzecs@gmail.com>
2022-03-06 20:15:52 +0100

    Added attachments

--- Webserver-Backend/node/index.js
+++ Webserver-Backend/node/index.js
@@ -1,12 +1,16 @@
 // REQUIRE
 const { setDefaultResultOrder } = require('dns');
-const express = require('express')
-const http = require('http')
+const express = require('express');
+const http = require('http');
+const FormData = require('form-data');
+const multer = require('multer');
+const uuid = require('uuid').v4;
+
 
 // VARIABLES
 const app = express();
 const PORT = 80;
-const databaseServerOptions = {
+const databaseServerOptionsPOST = {
     hostname: '192.168.19.6',
     port: 80,
     path: '/api/ticketsystem',
@@ -15,6 +19,34 @@ const databaseServerOptions = {
       'Content-Type': 'application/json'
     }
 }
+const databaseServerOptionsGET = {
+  hostname: '127.0.0.1',
+  port: 8080,
+  path: '/api/ticketsystem',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+var attachmentFileName = "";
+
+
+// MULTER SETTINGS
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+      const { originalname } = file;
+      attachmentFileName = `${uuid()}-${originalname}`;
+      cb(null, attachmentFileName);
+  }
+})
+const upload = multer({ storage });
+
+
+// FUNCTIONS
+
 
 // USING
 app.use(express.json());
@@ -28,13 +60,13 @@ app.listen(
 );
 
 
-// REDIRECT TO TICKETSYSTEM
+// REDIRECT TO FRONTEND
 app.get('/', (req, res) =>{
     res.redirect('/ticketsytem')
 })
 
 
-// SEND THE WEBSITE TO THE USER
+// SEND THE FRONTEND TO THE USER
 app.get('/ticketsystem', (req, res) =>{
     res.sendFile(__dirname + '/public/index.html')
 })
@@ -42,31 +74,50 @@ app.get('/ticketsystem', (req, res) =>{
 
 // HANDLE THE POST OF THE TICKET DATA
 // SEND THE PRIVIOUSLY COLLECTED DATA TO THE DATABASE SERVER'S API
-app.post('/ticketsystem', (req, res) =>{
-  res.sendStatus(200)  
+app.post('/ticketsystem', upload.single('attachment'), (req, res) =>{
+  var formData = new FormData(req.body);
+  var data = JSON.stringify(formData);
+  data = JSON.parse(data);  
+  data.filename = attachmentFileName;
+  console.log(data);
   
-  var data = req.body
-  console.log(data)
+  var postReq = http.request(databaseServerOptionsPOST, res => {
+    console.log(`statusCode: ${res.statusCode}`);
 
-  try {
-    const postReq = http.request(databaseServerOptions, res => {
-      console.log(`statusCode: ${res.statusCode}`)
+    res.on('data', d => {
+      process.stdout.write(d);
+    });   
+       
+    postReq.write(data);
+    postReq.end();
+  });
 
-      res.on('data', d => {
-        process.stdout.write(d)
-      })    
-
-      res.on('error', d => {
-        throw "connectionRefused_EXEPTION"
-      })
-    })         
-
-    postReq.write(JSON.stringify(data))
-    postReq.end() 
-
-  } catch (e) {
-    console.log('[ERROR]: Could not reach Database-Server')
-  }
-  res.end()
+  postReq.on('error', function(err) {
+    console.log();
+    console.log(err);
+    console.log();
+    res.sendStatus(500);
+  });    
 })
 
+app.get('/ticketsystem/tickets', (req, res) =>{
+  var getReq = http.request(databaseServerOptionsGET, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', d => {
+      process.stdout.write(d);
+    });   
+       
+    getReq.write(data);
+    getReq.end();
+  })
+  
+  getReq.on('error', function(err) {
+    console.log();
+    console.log(err);
+    console.log();
+    res.sendStatus(500);
+  });  
+
+  res.send(getReq.body);
+})
